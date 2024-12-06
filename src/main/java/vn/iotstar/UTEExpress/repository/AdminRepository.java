230f7874b2e06fa114f39bcb3e8af2cae3d85929
tien.blt <buitien747@gmail.com>
2024-12-06 10:54:30 +0700

    add admin repopsitory

---
+++ src/main/java/vn/iotstar/UTEExpress/repository/AdminRepository.java
@@ -0,0 +1,11 @@
+package vn.iotstar.UTEExpress.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import vn.iotstar.UTEExpress.entity.Manager;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Manager, String> {
+
+}
