0f305114a74084bcdf94e6d3d3b898d1464f1fcc
tien.blt <buitien747@gmail.com>
2024-12-14 01:19:18 +0700

    views customer info

--- src/main/java/vn/iotstar/UTEExpress/controllers/manager/ManagerCustomerController.java
+++ src/main/java/vn/iotstar/UTEExpress/controllers/manager/ManagerCustomerController.java
@@ -1,6 +1,9 @@
 package vn.iotstar.UTEExpress.controllers.manager;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,12 +15,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.servlet.http.HttpServletRequest;
 import vn.iotstar.UTEExpress.entity.Account;
 import vn.iotstar.UTEExpress.entity.Customer;
 import vn.iotstar.UTEExpress.entity.Manager;
+import vn.iotstar.UTEExpress.entity.Post;
+import vn.iotstar.UTEExpress.entity.Role;
 import vn.iotstar.UTEExpress.service.impl.AccountServiceImpl;
 import vn.iotstar.UTEExpress.service.impl.CustomerServiceImpl;
 import vn.iotstar.UTEExpress.service.impl.ManagerServiceImpl;
+import vn.iotstar.UTEExpress.service.impl.RoleServiceImpl;
 
 
 @Controller
@@ -29,6 +36,8 @@ public class ManagerCustomerController {
 	private CustomerServiceImpl customerService;
 	@Autowired 
 	private AccountServiceImpl accountService;
+	@Autowired
+	private RoleServiceImpl roleService;
 	
 	// trang co list cac customer thuoc buu cuc (dua tren city)
 	@GetMapping("customer-request")
@@ -83,6 +92,74 @@ public class ManagerCustomerController {
 		model.addAttribute("manager", manager);
 		
 		return "manager/customer";
+	}
+	
+	@GetMapping("add-customer")
+	public String addNewCustomerView(@PathVariable("id") Integer managerID, Model model) {
+		Manager manager = managerService.findById(managerID).get();
 		
+		if(manager == null) {
+			return "redirect:/";
+		}
+		model.addAttribute("manager", manager);
+		model.addAttribute("customer", new Customer());
+		
+		return "manager/customer-save";
+	}
+	
+	@GetMapping("delete-customer")
+	public String deleteCustomer(@PathVariable("id") Integer managerID, @RequestParam("username") String username){
+	 	// xóa customer
+		Customer customer = customerService.findCustomerByUserName(username);
+		customerService.delete(customer);
+		
+		// xóa account ứng vs customer
+		Account account = accountService.findById(username).get();
+		accountService.delete(account);
+		 
+		return "redirect:/manager/" + managerID + "/customers";
+	}
+	
+	@PostMapping("add-customer")
+	public String addNewCustomer(@PathVariable("id") Integer managerID, @ModelAttribute("customer") Customer customer,
+														HttpServletRequest request) {
+		Manager manager = managerService.findById(managerID).get();
+		
+		if(manager == null) {
+			return "redirect:/";
+		}
+		
+		// lấy các tham số từ form
+		String birthString = request.getParameter("birth");
+		// Manually parse the birth string to Date
+		
+	    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+	    Date birthDate = null;
+	    try {
+	        birthDate = dateFormat.parse(birthString);
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return "redirect:/manager/" + managerID + "/manager-info?status=invalid-birth-date"; // Handle invalid date
+	    }
+	    customer.setBirth(birthDate);
+	    
+	 // Create account
+	    Optional<Account> optionalAccount = accountService.findById(request.getParameter("username"));
+	    if (optionalAccount.isPresent()) {
+	        return "redirect:/manager/" + managerID + "/add-shipper?error=existUsername";
+	    }
+	    
+	    Account account = new Account();
+	    Role role = roleService.findRoleByRoleNameIgnoreCase("CUSTOMER");
+	    account.setRole(role);
+	    account.setUsername(request.getParameter("username"));
+	    account.setPassword(customer.getPassword());
+	    accountService.save(account);
+	  
+	    customer.setAccount(account);
+	    
+	    customerService.save(customer);
+		
+		return "redirect:/manager/" + managerID + "/customers";
 	}
 }
--- src/main/java/vn/iotstar/UTEExpress/controllers/manager/ManagerOrderController.java
+++ src/main/java/vn/iotstar/UTEExpress/controllers/manager/ManagerOrderController.java
@@ -1,5 +1,36 @@
 package vn.iotstar.UTEExpress.controllers.manager;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import vn.iotstar.UTEExpress.entity.Manager;
+import vn.iotstar.UTEExpress.service.impl.ManagerServiceImpl;
+import vn.iotstar.UTEExpress.service.impl.OrderServiceImpl;
+
+@Controller
+@RequestMapping("/manager/{id}/")
 public class ManagerOrderController {
+	@Autowired
+	private ManagerServiceImpl managerService;
+	@Autowired
+	private OrderServiceImpl orderService;
+	
+	public String allOrderOfPost(@PathVariable("id") Integer managerID, Model model) {
+		Manager manager = managerService.findById(managerID).get();
+		
+		model.addAttribute("manager",manager);
+		
+		// order co kieu giao hàng tiết kiệm 
+		
+		// order co kieu giao hàng nhanh
+		
+		// order co kieu giao hàng hỏa tốc
+		
+		
+		return "manager/order";
+	}
 
 }
