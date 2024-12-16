1af181fe0896999aa930c53de044da5ea58b8648
Cloudy1112 <chonglevan2004@gmai.com>
2024-12-16 21:44:43 +0700

    Header for edit profile

---
+++ src/main/java/vn/iotstar/UTEExpress/controllers/admin/AdminController.java
@@ -0,0 +1,16 @@
+package vn.iotstar.UTEExpress.controllers.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+	@GetMapping("")
+	public String AdminHome(@RequestParam String param) {
+		return new String();
+	}
+	
+}
